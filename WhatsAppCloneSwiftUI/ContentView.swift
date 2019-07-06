550b7827eb295542ed0ec63f84abb74b6d5fdea2
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-07-06 15:41:00 +0300

    Initial Commit

---
+++ WhatsAppCloneSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WhatsAppCloneSwiftUI
+//
+//  Created by Atil Samancioglu on 6.07.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
